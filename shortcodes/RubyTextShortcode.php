0e7aa04e15b10a808a81e56613f27364ffdbce17
François VIDIT <tidiview@users.noreply.github.com>
2017-09-02 23:13:34 +0200

    Update RubyTextShortcode.php

--- shortcodes/RubyTextShortcode.php
+++ shortcodes/RubyTextShortcode.php
@@ -10,7 +10,7 @@ class RubytextShortcode extends Shortcode
         $this->shortcode->getHandlers()->add('ruby', function(ShortcodeInterface $sc) {
             $rubyx = $sc->getContent();
             $ruby = $sc->getParameter('ruby', trim($sc->getParameterAt(0), ':'));
-            $rubytext = '<ruby><rb>'.$rubyx.'</rb><rp>（</rp><rt>'.$rubyx.'</rt><rp>）</rp></ruby>';
+            $rubytext = '<ruby><rb>'.$rubyx.'</rb><rp>（</rp><rt>'.$ruby.'</rt><rp>）</rp></ruby>';
             return $rubytext;
         });
     }
