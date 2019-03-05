0ba2820f353f41735b15d8af4d9dc769f111b7db
Richard Hainsworth <finanalyst@users.noreply.github.com>
2019-03-05 12:54:15 +0800

    Concatenate 'px' to $size only if $size is numeric
    Non-numeric values of size, eg 'x-large', get passed through without alteration

--- shortcodes/SizeShortcode.php
+++ shortcodes/SizeShortcode.php
@@ -9,7 +9,8 @@ class SizeShortcode extends Shortcode
     {
         $this->shortcode->getHandlers()->add('size', function(ShortcodeInterface $sc) {
             $size = $sc->getParameter('size', $sc->getBbCode());
-            return '<span style="font-size: '.$size.'px;">'.$sc->getContent().'</span>';
+            if ( is_numeric($size) ) { $size = $size.'px' ; }
+            return '<span style="font-size: '.$size.';">'.$sc->getContent().'</span>';
         });
     }
 }
