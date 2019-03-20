137ec2062fe395f2c1e2424841976d8f67c553f9
Oskar Carl <Kalagon@users.noreply.github.com>
2019-03-20 17:39:17 +0000

    add Fontawesome 5 support (#56)

--- shortcodes/FontAwesomeShortcode.php
+++ shortcodes/FontAwesomeShortcode.php
@@ -13,6 +13,11 @@ class FontAwesomeShortcode extends Shortcode
             if ($this->config->get('plugins.shortcode-core.fontawesome.load', false)) {
                 $this->shortcode->addAssets('css', $this->config->get('plugins.shortcode-core.fontawesome.url'));
             }
+            if ($this->config->get('plugins.shortcode-core.fontawesome.v5', false)) {
+                $v5classes = array('fab', 'fal', 'fas', 'far');
+            } else {
+                $v5classes = array();
+            }
 
             // Get shortcode content and parameters
             $str = $sc->getContent();
@@ -22,18 +27,24 @@ class FontAwesomeShortcode extends Shortcode
                 $icon = 'fa-'.$icon;
             }
 
-            if ($icon) {
-                $extras = explode(',',$sc->getParameter('extras', ''));
-                foreach ($extras as $extra) {
-                    if (!empty($extra)) {
-                        if (!Utils::startsWith($extra, 'fa-')) {
-                            $extra = 'fa-'.$extra;
+            if($icon) {
+                $fa_class = 'fa';
+                $extras = explode(',', $sc->getParameter('extras', ''));
+
+                foreach($extras as $extra) {
+                    if(!empty($extra)) {
+                        if(in_array($extra, $v5classes)) {
+                            $fa_class = $extra;
+                            continue;
                         }
-                        $icon .= ' '.$extra;
+                        if(!Utils::startsWith($extra, 'fa-')) {
+                            $extra = 'fa-' . $extra;
+                        }
+                        $icon .= ' ' . $extra;
                     }
                 }
 
-                $output = '<i class="fa '.$icon.'">'.$str.'</i>';
+                $output = '<i class="' . $fa_class . ' ' . $icon . '">' . $str . '</i>';
 
                 return $output;
             }
