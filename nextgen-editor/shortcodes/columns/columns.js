de3d1f821ff3cd3688ce8e22369b4150f819294d
Andy Miller <rhuk@mac.com>
2020-10-16 15:30:05 -0600

    new icons for dropdown

--- nextgen-editor/shortcodes/columns/columns.js
+++ nextgen-editor/shortcodes/columns/columns.js
@@ -5,6 +5,7 @@ window.nextgenEditor.addShortcode('columns', {
   button: {
     group: 'shortcode-core',
     label: 'Columns',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22 0H2a2 2 0 00-2 2v20a2 2 0 002 2h20a2 2 0 002-2V2a2 2 0 00-2-2zm-7 2.5v19a.5.5 0 01-.5.5h-5a.5.5 0 01-.5-.5v-19a.5.5 0 01.5-.5h5a.5.5 0 01.5.5zM2.5 2h4a.5.5 0 01.5.5v19a.5.5 0 01-.5.5h-4a.5.5 0 01-.5-.5v-19a.5.5 0 01.5-.5zm19 20h-4a.5.5 0 01-.5-.5v-19a.5.5 0 01.5-.5h4a.5.5 0 01.5.5v19a.5.5 0 01-.5.5z"/></svg>',
   },
   attributes: {
     count: {
--- nextgen-editor/shortcodes/details/details.js
+++ nextgen-editor/shortcodes/details/details.js
@@ -5,6 +5,7 @@ window.nextgenEditor.addShortcode('details', {
   button: {
     group: 'shortcode-core',
     label: 'Details',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.2 8.348a1 1 0 001.4.2l3.465-2.6a1.5 1.5 0 000-2.4L3.6.948a1 1 0 00-1.2 1.6l2.666 2a.25.25 0 010 .4l-2.666 2a1 1 0 00-.2 1.4zM23 21.248H1a1 1 0 000 2h22a1 1 0 000-2zM23 16.248H1a1 1 0 000 2h22a1 1 0 000-2zM23 11.248H1a1 1 0 000 2h22a1 1 0 000-2zM24 7.248a1 1 0 00-1-1H12a1 1 0 000 2h11a1 1 0 001-1zM12 3.248h11a1 1 0 000-2H12a1 1 0 000 2z"/></svg>',
   },
   attributes: {
     summary: {
